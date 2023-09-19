c4ff227b71a0258072d8fed2f0fa26449ca00567
EUNSUN <sunej10@naver.com>
2023-09-20 03:01:53 +0900

    [정은선] - 최초 파일 추가

---
+++ src/test/java/com/sunny/jungstagram/JungstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunny.jungstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JungstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
